48f6a57bea59ccece8d4bbe4c54a4b8e9d38530f
turtton <top.gear7509@turtton.net>
2024-10-04 09:39:42 +0900

    feat: Change default players_sleeping_percentage to 50

---
+++ src/main/java/net/yukulab/mixin/MixinServerWorld.java
@@ -0,0 +1,32 @@
+package net.yukulab.mixin;
+
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.WorldGenerationProgressListener;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.random.RandomSequencesState;
+import net.minecraft.world.GameRules;
+import net.minecraft.world.dimension.DimensionOptions;
+import net.minecraft.world.level.ServerWorldProperties;
+import net.minecraft.world.level.storage.LevelStorage;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+import java.util.concurrent.Executor;
+
+@Mixin(ServerWorld.class)
+abstract public class MixinServerWorld {
+    @Inject(
+            method = "<init>",
+            at = @At("RETURN")
+    )
+    private void changeDefaultSleepPercentage(MinecraftServer server, Executor workerExecutor, LevelStorage.Session session, ServerWorldProperties properties, RegistryKey worldKey, DimensionOptions dimensionOptions, WorldGenerationProgressListener worldGenerationProgressListener, boolean debugWorld, long seed, List spawners, boolean shouldTickTime, RandomSequencesState randomSequencesState, CallbackInfo ci) {
+        var sleepPercentage = properties.getGameRules().get(GameRules.PLAYERS_SLEEPING_PERCENTAGE);
+        if (sleepPercentage.get() == 100) {
+            sleepPercentage.set(50, server);
+        }
+    }
+}
